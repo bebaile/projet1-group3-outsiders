dc283f52f181b1fbf9e9ecf2a883e7cd740a1bf2
Basile Carle <basile.carle@edhec.com>
2022-03-24 17:24:33 +0100

    modification carte formateur + script séparé pour apropos

---
+++ assets/script-apropos.js
@@ -0,0 +1,7 @@
+document.querySelector('.tutor').addEventListener('mouseover', function() {
+    document.querySelector('.our-tutor-card').style.display = 'flex';
+  });
+  
+  document.querySelector('.window-button').addEventListener('click', function() {
+    document.querySelector('.our-tutor-card').style.display = 'none';
+  });
--- script.js
+++ script.js
@@ -124,15 +124,6 @@ fetch("./assets/equipe.json")
 
 
 
-// js pour la page apropos
 
-
-document.querySelector('.tutor').addEventListener('mouseover', function() {
-  document.querySelector('.our-tutor-card').style.display = 'flex';
-});
-
-document.querySelector('.tutor').addEventListener('mouseleave', function() {
-  document.querySelector('.our-tutor-card').style.display = 'none';
-});
 
 
